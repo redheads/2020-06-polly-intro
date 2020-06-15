ef04e96d2967681e92ca8462f5020576abbaf552
Patrick Drechsler <patrick.drechsler@redheads.de>
2020-06-15 20:35:54 +0200

    code: happy case (no resilience needed)

--- code/csharp/Demo/Demo.Tests/Demo0Tests.cs
+++ code/csharp/Demo/Demo.Tests/Demo0Tests.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using NSubstitute;
 using Xunit;
 
 namespace Demo.Tests
@@ -6,9 +7,17 @@ namespace Demo.Tests
     public class Demo0Tests
     {
         [Fact]
-        public void Test1()
+        public void IsAdult_happy_case()
         {
-            true.Should().BeTrue();
+            // Arrange
+            var userService = Substitute.For<IUserService>();
+            userService.GetAge(Arg.Any<int>()).Returns(42);
+            
+            // Act
+            var sut = new Demo0(userService);
+
+            // Assert
+            sut.IsAdult(1).Should().BeTrue();
         }
     }
 }
